62ad6b54715572b6405d5c8d36047c72f43653e5
hsendirani <dirani.hussein93@gmail.com>
2026-07-05 12:08:52 +0200

    Initial Spring WebFlux project setup

---
+++ src/test/java/com/nordea/bookflow/BookflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordea.bookflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
